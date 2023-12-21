2af147d2eb223c2a9b417c213ec37862418cfca6
magno-brito <magnoleal22@gmail.com>
2023-12-21 16:13:47 -0300

    First commit

---
+++ gui.py
@@ -0,0 +1,13 @@
+
+import customtkinter
+
+app = customtkinter.CTk()
+app.geometry("600x600")
+app.title("CRUD Using Python and MongoDB")
+app.configure(fg_color="green")
+app.resizable(height=False, width=False)
+
+app.mainloop()
+
+
+
